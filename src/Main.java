1b18e81ab2ab97cb6e70f55f32577fe627d01f1c
zdiesing <zdiesing2480@student.whatcom.edu>
2020-01-22 10:52:49 -0800

    Main is now a Hello World program for JavaFX, make sure it runs for you

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,25 @@
-public class Main
-{
-	public static void main(String[] args)
-	{
-		Chess chess = new Chess();
-		chess();
-	}
+// Stole this Hello World program from online
+// Use this to test if JavaFX works for you
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("My First JavaFX App");
+        
+        Label label = new Label("Hello World, JavaFX !");
+        Scene scene = new Scene(label, 400, 200);
+        primaryStage.setScene(scene);
+        
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
 }
