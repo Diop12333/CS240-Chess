ee8a2b0778e51f7dfa79de515733fd7f10f3acec
zdiesing <zdiesing2480@student.whatcom.edu>
2020-02-04 10:51:34 -0800

    Connect piece to board - class is still WIP, but foundation is here

--- src/chess/Piece.java
+++ src/chess/Piece.java
@@ -8,14 +8,24 @@ public abstract class Piece {
 	private static String whiteImgFileName;
 	private static String blackImgFileName;
 	
+	private Board board;
+	private Coordinate coord;
+	
 	private Image img;
 	
-	public Piece(boolean white) {
+	public Piece(Board board, Coordinate coord, boolean white) {
+		this.board = board;
+		this.coord = coord;
+		
+		// add piece to board here
+		
 		if (white) img = new Image(imgFolder + whiteImgFileName);
 		else img = new Image(imgFolder + blackImgFileName);
 	}
 	
 	public Image getImg() { return img; }
+	
+	public void move(Coordinate newCoord) {} // TODO
 	
 	// Returns coordinates piece can move to
 	public abstract Coordinate[] validMoves();
