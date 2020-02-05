84a51f21f7d6d400aba072cc232b65a5c49bf2ae
Dimitri Schneider <qKymQ6yd5aMG9pdD@protonmail.com>
2020-02-05 10:10:18 -0800

    modified:   src/chess/Pawn.java

--- src/chess/Pawn.java
+++ src/chess/Pawn.java
@@ -1,11 +1,16 @@
 package chess;
 
-import java.util.LinkedList;
+import java.util.*;
 
-public class Pawn extends Movement
+public class Pawn
 {
+	private Movement DESTINATION = null;
+	private Movement PIECE = 'p';
+	
 	public Pawn()
-	{
-		LinkedList<Node> movement = new LinkedList<Node>();
+	{		
+		char piece = 'p';
 	}
+	
+	
 }
